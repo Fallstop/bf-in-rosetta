de3b0c5193142e42ef7e823c2ade20cd4083429f
Fallstop <jasper@ngita.co.nz>
2020-02-26 21:53:23 +1300

    Rust bracket nested level done

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -3,6 +3,8 @@ use std::fs;
 use regex::Regex;
 use std::process;
 
+
+
 fn main() {
     
     println!("\n\nRunning\n");
@@ -13,6 +15,7 @@ fn main() {
 
 fn process_bf(args: Vec<String>){
     if args.len() != 2  {
+        println!("How to use: main [filename]");
         let error_message = format!("Args not correct, 1 expected, {} recived", args.len());
         throw_error(10, error_message);
         
@@ -24,9 +27,11 @@ fn process_bf(args: Vec<String>){
     let code_pre: Vec<char> = file_contents.chars().collect();
     let mut code_post: Vec<char> = vec![];
     
-    code_post = regex_scan(code_pre);
+    code_post = match_braces(regex_scan(code_pre));
     let code_print: String  = code_post.into_iter().collect();
     println!("Code post scaning: {}",code_print);
+
+    
 }
 fn regex_scan(code_pre: Vec<char>) -> Vec<char>{
     let regex_code = Regex::new("^[\\[\\]<>+-.,,]$").unwrap();
@@ -37,6 +42,22 @@ fn regex_scan(code_pre: Vec<char>) -> Vec<char>{
             code_post.push(current_char);
         }
         
+    }
+    return code_post;
+}
+
+fn match_braces(code_post: Vec<char>)-> Vec<char>{
+    let mut nested_level: i8 = 0;
+    for i in 0..code_post.len(){
+        if code_post[i].encode_utf8(&mut [1]) == "["{
+            nested_level -=- 1;
+            println!("Found Left braket, nested level: {}",nested_level)
+        }
+        else if code_post[i].encode_utf8(&mut [1]) == "]"{
+            
+            println!("Found Left braket, nested level: {}",nested_level);
+            nested_level -= 1;
+        }
     }
     return code_post;
 }
