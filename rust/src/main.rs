75813e83b305030474b7e3b2cd8792aad5e7f158
Fallstop <jasper@ngita.co.nz>
2020-02-27 20:26:08 +1300

    RUST start on running of bf

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -26,7 +26,7 @@ fn process_bf(args: Vec<String>){
     //println!("BF code:\n{}",file_contents);
     let code_pre: Vec<char> = file_contents.chars().collect();
     let code_post: Vec<char>;
-    
+   
     code_post = match_braces(regex_scan(code_pre));
     let code_print: String  = code_post.into_iter().collect();
     println!("Code post scaning: {}",code_print);
@@ -46,19 +46,49 @@ fn regex_scan(code_pre: Vec<char>) -> Vec<char>{
     return code_post;
 }
 
+fn run_bf(code: Vec<char>,braces: Vec<Vec<i32>>){
+    println!("Running bf code");
+    let mut memory: Vec<i64> = vec!();
+    let mut code_pointer: usize = 0;
+    while code_pointer < code.len() as usize{
+        match code[code_pointer] {
+            "." => println!(". has been found"),
+            _ => println!("bf symbole not reconised"),
+
+		}
+	}
+}
+
 fn match_braces(code_post: Vec<char>)-> Vec<char>{
-    let mut nested_level: i8 = 0;
+    let mut nested_level: i32 = 0;
+    let mut bracket_link: Vec<Vec<i32>> = vec!(vec!());
     for i in 0..code_post.len(){
         if code_post[i].encode_utf8(&mut [1]) == "["{
             nested_level -=- 1;
-            println!("Found Left braket, nested level: {}",nested_level)
+            println!("Found Left braket, nested level: {}, the charcture location is {}",nested_level,i);
+            bracket_link.push(vec!(0,nested_level,i as i32));
         }
         else if code_post[i].encode_utf8(&mut [1]) == "]"{
             
-            println!("Found Left braket, nested level: {}",nested_level);
+            println!("Found Right braket, nested level: {}",nested_level);
+            let mut x: usize =  bracket_link.len() -1;
+            
+            'scan_for_match: while x > 0{
+                if bracket_link[x][0] == 0 &&  bracket_link[x][1] == nested_level{
+                    bracket_link.push(vec!(1,nested_level,bracket_link[x][2]));
+                    
+                    println!("{} {} {}",i,x,bracket_link[x][2]);
+
+                    break 'scan_for_match;
+                    
+				}
+                x -= 1;
+			}
             nested_level -= 1;
         }
     }
+    println!("{}",bracket_link[2][2]);
+    println!("{:?}",bracket_link);
     return code_post;
 }
 
