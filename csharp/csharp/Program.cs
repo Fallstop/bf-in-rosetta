61fbfc51c36a474ed3b9e3831b2954b2cd60963e
Nathan Hare <nathanhare32@gmail.com>
2020-05-10 15:47:30 +1200

    C# go brrr

--- csharp/csharp/Program.cs
+++ csharp/csharp/Program.cs
@@ -20,8 +20,15 @@ namespace csharp
             }
 
             String file = OpenFile(args[0]);
-
-            Execute(file);
+            if (args.Length == 1)
+            {
+                Execute(file);
+            }
+            else
+            {
+                String inputs = OpenFile(args[1]);
+                Execute(file, inputs);
+            }
         }
 
         static String OpenFile(String source) 
@@ -59,7 +66,7 @@ namespace csharp
             }
 
             int memoryPointer = 0;
-            int codePointer = 9;
+            int codePointer = 0;
             Stack braces = new Stack();
             while (codePointer < src.Count)
             {
@@ -93,7 +100,7 @@ namespace csharp
                         braces.Push(codePointer - 1);
                         break;
                     case BrainFuck.JNZ:
-                        if (memory[codePointer] != 0)
+                        if (memory[memoryPointer] != 0)
                         {
                             codePointer = (int) braces.Pop();
                         }
@@ -107,7 +114,79 @@ namespace csharp
                 codePointer++;
             }
         }
+        
+        static void Execute(String source, String inputs)
+        {
+            ArrayList src = Parse(source);
+            ArrayList input = ParseCSV(inputs);
+            int inputPointer = 0;
+            const int memorySize = 3000;
+            char[] memory = new char[memorySize];
+            for (int i = 0; i < memorySize - 1; i++)
+            {
+                memory[i] = '\0';
+            }
 
+            int memoryPointer = 0;
+            int codePointer = 0;
+            Stack braces = new Stack();
+            while (codePointer < src.Count)
+            {
+                BrainFuck token = (BrainFuck) src[codePointer];
+                switch (token)
+                {
+                    case BrainFuck.INC:
+                        memory[memoryPointer]++;
+                        break;
+                    case BrainFuck.DEC:
+                        memory[memoryPointer]--;
+                        break;
+                    case BrainFuck.SLF:
+                        memoryPointer--;
+                        break;
+                    case BrainFuck.SRT:
+                        memoryPointer++;
+                        if (memoryPointer > memorySize)
+                        {
+                            memoryPointer = 0;
+                        }
+
+                        break;
+                    case BrainFuck.IN:
+                        try
+                        {
+                            memory[memoryPointer] = (char) input[inputPointer];
+                            Console.WriteLine(input[0]);
+                            inputPointer++;
+                        }
+                        catch (Exception exception)
+                        {
+                            Console.Error.Write("Error: Not enough inputs provided in csv, falling back to on-demand input");
+                            memory[memoryPointer] = Input();
+                        }
+                        break;
+                    case BrainFuck.OUT:
+                        Console.WriteLine((int) memory[memoryPointer]);
+                        break;
+                    case BrainFuck.SJP:
+                        braces.Push(codePointer - 1);
+                        break;
+                    case BrainFuck.JNZ:
+                        if (memory[memoryPointer] != 0)
+                        {
+                            codePointer = (int) braces.Pop();
+                        }
+                        else
+                        {
+                            braces.Pop();
+                        }
+                        break;
+                }
+                
+                codePointer++;
+            }
+        }
+        
         static ArrayList Parse(String source)
         {
             ArrayList code = new ArrayList();
@@ -154,6 +233,41 @@ namespace csharp
             }
             Console.Write("\n");
             return code;
+        }
+
+        static ArrayList ParseCSV(String input)
+        {
+            Console.WriteLine($"Parsing inputs: {input}");
+            ArrayList list = new ArrayList();
+            StringBuilder sb = new StringBuilder();
+            foreach (char c in input)
+            {
+                if (c < 57 && c > 47)
+                {
+                    sb.Append(c);
+                }
+
+                if (c == ',' && sb.Length > 0)
+                {
+                    Console.WriteLine(sb.ToString());
+                    char val = (char) Int32.Parse(sb.ToString());
+                    list.Add(val);
+                }
+            }
+            
+            char val2 = (char) Int32.Parse(sb.ToString());
+            list.Add(val2);
+            
+            Console.Write("Parsed inputs: ");
+            
+            foreach (var o in list)
+            {
+                var y = (char) o;
+                Console.Write($"{(int) y}, ");
+            }
+
+            Console.Write("\n");
+            return list;
         }
 
         static char Input()
