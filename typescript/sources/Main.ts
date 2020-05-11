0cb2f528fc528d1b783cfec1c8492f875050ceba
Nathan Hare <nathanhare32@gmail.com>
2020-05-11 19:22:21 +1200

    Thats enough for tonight

--- typescript/sources/Main.ts
+++ typescript/sources/Main.ts
@@ -1,6 +1,7 @@
 import * as process from "process";
 import * as fs from 'fs';
-import {readFileSync} from "fs";
+
+const tokenIndex = ["+", "-", ",", ".", "<", ">", "[", "]"];
 
 enum BrainFuck {
     INC = 0,
@@ -14,8 +15,9 @@ enum BrainFuck {
 }
 
 const parse = function (source: String): Array<BrainFuck> {
-    console.log(`Parsing ${source}`);
+    console.log(`Parsing: ${source}`);
     let tokens = new Array<BrainFuck>();
+    // @ts-ignore
     const char = [...source];
     char.forEach(char => {
         switch (char) {
@@ -47,30 +49,73 @@ const parse = function (source: String): Array<BrainFuck> {
                 break;
         }
     });
-
+    process.stdout.write("Parsed output: ");
+    tokens.forEach(token => {
+        process.stdout.write(tokenIndex[token]);
+    });
+    process.stdout.write("\n");
     return tokens;
 }
 
-const execute = function(sourceFile: String) {
+const execute = async function(sourceFile: String) {
     let source = parse(sourceFile);
+    const memorySize = 30000;
+    let memory = new Int8Array(memorySize);
+    for (let i = 0; i < memorySize - 1; i++) {
+        memory[i] = 0;
+    }
+    let mp = 0;
+    let braces = new Array<number>();
+
+    let i = 0;
+
+    let qurestion = [
+        {
+            type: 'input',
+            name: 'name',
+            message: '>>> '
+        }
+    ]
+
+    while (i < source.length) {
+        let token = source[i];
+        switch (token) {
+            case BrainFuck.OUT:
+                console.log(memory[mp]);
+                break;
+            case BrainFuck.IN:
+                
+                break;
+            default:
+                break;
+        }
+        i++;
+    }
+
+    console.log("Done!");
 }
 
-const args = process.argv;
+const main = function() {
 
-if (args.length < 3) {
-    console.error("Error, expected at lease 1 argument, got none\nUsage: npm execute [source_file]");
-    process.exit(-1);
+    const args = process.argv;
+
+    if (args.length < 3) {
+        console.error("Error, expected at lease 1 argument, got none\nUsage: npm execute [source_file]");
+        process.exit(-1);
+    }
+
+    let source: String;
+
+    try {
+        source = fs.readFileSync(args[2], "utf-8")
+    } catch (e) {
+        console.error("Unable to open file " + args[2]);
+        process.exit(-1);
+    }
+
+    if (args.length < 4) {
+        execute(source);
+    }
 }
 
-let source: String;
-
-try {
-    source = fs.readFileSync(args[2], "utf-8")
-} catch (e) {
-    console.error("Unable to open file " + args[2]);
-    process.exit(-1);
-}
-
-if (args.length < 4) {
-    execute(source);
-}
+main();
