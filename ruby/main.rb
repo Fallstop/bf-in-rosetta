e016999253e518b8f2d0e4ff84aa8f04e9eacd4c
Nathan Hare <nathanhare32@gmail.com>
2020-05-16 16:44:41 +1200

    brrrrrrrr

--- ruby/main.rb
+++ ruby/main.rb
@@ -102,11 +102,58 @@ def execute(source)
   puts 'Done'
 end
 
+def execute_csv(source, inputs)
+  tokens = parse source
+  input_pointer = 0
+  input = parse_csv(input)
+  i = 0
+  memory_size = 30_000
+  memory_size = memory_size.freeze
+  memory = Array.new(memory_size) { |i| 0 }
+  memory_pointer = 0
+  braces = []
+  puts 'Executing'
+  while i < tokens.length
+    case tokens[i]
+    when BrainFuck::INC
+      memory[memory_pointer] += 1
+    when BrainFuck::DEC
+      memory[memory_pointer] -= 1
+    when BrainFuck::IN
+      memory[memory_pointer] = input[input_pointer] if input_pointer < input.length else puts "Error not enough inputs provided, falling back to console input"; input
+    when BrainFuck::OUT
+      puts "Output: #{memory[memory_pointer]}"
+    when BrainFuck::SLF
+      memory_pointer -= 1
+      memory_pointer = memory_size - 1 if memory_pointer.negative?
+    when BrainFuck::SRT
+      memory_pointer += 1
+      memory_pointer = 0 if memory_pointer > memory_size
+    when BrainFuck::SJP
+      braces.push i - 1
+    when BrainFuck::JNZ
+      if memory[memory_pointer] != 0
+        i = braces.pop
+      else
+        _ = braces.pop
+      end
+    end
+    i += 1
+  end
+  puts 'Done'
+end
+
 if ARGV.empty?
   puts 'Expected at lease 1 argument got none'
-  exit -1;
+  exit -1
 end
 
 sources = open_file ARGV[0]
+if ARGV.length < 1
+  execute sources
+  exit 0
+end
 
-execute sources
+inputs = open_file ARGV[1]
+
+execute_csv sources, inputs
