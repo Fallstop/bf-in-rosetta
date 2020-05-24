a121af07e1a139024043ef7a06ac2ed025d2a027
Jasper M-W <jasper@ngita.co.nz>
2020-05-24 19:03:39 +1200

    New comments + actually enabled optimizations

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -10,19 +10,18 @@ use std::io::{self, Write,stdout};
 fn main() {
     let start_time = Instant::now();
     let args: Vec<String> = env::args().collect();
-    let mut config = get_config(&args);
-    // let macro_code = code;
+    let mut config = get_config(&args); //Takes the args and returns a ConfigStruct with the processed Code, Inputs and default options
     if config.code_compression == true {
-        config.code = macro_scan(&config.code); //Condenses repeated charcters in macros (Shortcuts) 
+        config.code = macro_scan(&config.code); //Condenses repeated characters into macros (Shortcuts) a=> b=< c=+ d=-
     }
-    config.braces = match_braces(&config); //Matches the loops in the code
+    config.braces = match_braces(&config); //Precalculates the nested loops/braces in the code
     run_bf(&config); //Steps through processed code
     let elapsed = start_time.elapsed();
     log(&config,format!("Time taken: {:.5?}", elapsed),2);
 }
 
 
-fn process_bf(filename: &String) -> Vec<char>{
+fn process_bf(filename: &String) -> Vec<char>{ //Read from file and remove non-command characters
     let file_contents = fs::read_to_string(filename)
         .expect("Something went wrong reading the file");
     let code_pre: Vec<char> = file_contents.chars().collect();
@@ -34,11 +33,9 @@ fn process_bf(filename: &String) -> Vec<char>{
             code_post.push(current_char);
         }
     }
-
     return code_post;
 }
-fn macro_scan(code: &Vec<char>) -> Vec<char>{
-    
+fn macro_scan(code: &Vec<char>) -> Vec<char>{ //Condenses sequential characters 3-9 chars long.
     let mut code_macro: Vec<char>=vec!();
     let mut char_list: Vec<char>=vec!('0','0','0');
     let mut i: usize=0;
@@ -79,11 +76,19 @@ fn macro_scan(code: &Vec<char>) -> Vec<char>{
     return code_macro;
 
 }
-fn equal_vec(arr: &Vec<char>) -> bool {
+fn equal_vec(arr: &Vec<char>) -> bool { //Used in the macro-code to make sure 3 characters are the same
     arr.iter().min() == arr.iter().max()
 }
 
-fn run_bf(config: &ConfigStruct){
+fn run_bf(config: &ConfigStruct){ //Primary runtime - Run after all preparations
+    /*
+    Takes the config with all the data required
+
+    Loop cache routine
+        When the normal routine encounters the start of a loop, instead of doing nothing, if enabled,
+        it looks at the caching_reference to see if it needs to either: Start a thread to calculate the
+        loop or use the already made cache at this address or do nothing as usual.
+    */
     log(&config,format!("Running bf code"),2);
     let code = config.code.clone();
     let mut inputs = config.inputs.clone();
@@ -93,10 +98,10 @@ fn run_bf(config: &ConfigStruct){
     let mut code_pointer: usize = 0;
     let mut inputs_pointer: usize = 0;
     let caching_data = Arc::new(Mutex::new(vec!()));
-    let caching_refrence = Arc::new(Mutex::new(vec!(0; code.len())));
+    let caching_reference = Arc::new(Mutex::new(vec!(0; code.len())));
     let mut handles = vec![];
     let code_arc = Arc::new(code.clone());
-    log(&config,format!("Varibles Initialesd"),3);
+    log(&config,format!("Variables Initialized"),3);
     if config.output_type == 'a'{
         log_without_newline(&config,format!("Output: "),2);
     }
@@ -117,8 +122,8 @@ fn run_bf(config: &ConfigStruct){
             'c' => {code_pointer+=1; memory[memory_pointer]+=code[code_pointer] as i64;}, //+
             'd' => {code_pointer+=1; memory[memory_pointer]-=code[code_pointer] as i64; }, //-
             '[' => {
-                if config.code_loop_cache == true {
-                    let arc_cache_status = Arc::clone(&caching_refrence);
+                if config.code_loop_cache == true { //If loop caching is enabled
+                    let arc_cache_status = Arc::clone(&caching_reference);
                     let mut mutex_cache_status = arc_cache_status.lock().unwrap();
                     let current_cache_status = mutex_cache_status[code_pointer];
                     if current_cache_status == 0 {
@@ -126,7 +131,7 @@ fn run_bf(config: &ConfigStruct){
                         drop(mutex_cache_status);
                         let caching_data = Arc::clone(&caching_data);
                         let code_arc = Arc::clone(&code_arc);
-                        let caching_refrence = Arc::clone(&caching_refrence);
+                        let caching_reference = Arc::clone(&caching_reference);
                         let mut code_pointer_local = code_pointer.clone()+1;
                         let handle = thread::spawn(move || {
                             thread::park_timeout(Duration::from_millis(10));
@@ -134,9 +139,9 @@ fn run_bf(config: &ConfigStruct){
                             let mut code_arc_char = code_arc[code_pointer_local];
                             let mut able_to_be_cached: bool = true;
                             let starting_position = code_pointer_local.clone();
-                            let mut mutex_caching_refrence = caching_refrence.lock().unwrap();
-                            mutex_caching_refrence[starting_position] = -1;
-                            drop(mutex_caching_refrence);
+                            let mut mutex_caching_reference = caching_reference.lock().unwrap();
+                            mutex_caching_reference[starting_position] = -1;
+                            drop(mutex_caching_reference);
                             while code_arc_char != ']' && able_to_be_cached == true{
                                 match code_arc_char {
                                     '<' => current_cache.memory_pointer -=1,
@@ -174,23 +179,23 @@ fn run_bf(config: &ConfigStruct){
                                 current_cache.code_pointer = code_pointer_local as i32;
                                 current_cache.loop_starting_loc = starting_position as i32;
                                 let mut mutex_caching_data = caching_data.lock().unwrap();
-                                let mut mutex_caching_refrence = caching_refrence.lock().unwrap();
+                                let mut mutex_caching_reference = caching_reference.lock().unwrap();
                                 mutex_caching_data.push(current_cache);
-                                mutex_caching_refrence[starting_position-1] = mutex_caching_data.len() as i32; //One more than actual index
+                                mutex_caching_reference[starting_position-1] = mutex_caching_data.len() as i32; //One more than actual index
                                 drop(mutex_caching_data);
                                 drop(caching_data);
-                                drop(mutex_caching_refrence);
+                                drop(mutex_caching_reference);
                             }
-                            else { // Error caused cache to not compleate
+                            else { // Loop is not possible to cache
                                 drop(caching_data);
                             }
 
                         });
                         handles.push(handle);
-                    } else if current_cache_status == -30{
+                    } else if current_cache_status > 0{ //Loop has been cached
                         let mutex_cache = Arc::clone(&caching_data);
                         let unlocked_cache = mutex_cache.lock().unwrap();
-                        let cache = unlocked_cache[current_cache_status as usize-1].clone();
+                        let cache = unlocked_cache[current_cache_status as usize-1].clone(); //Gets the Cache obj with all the necessary info
                         drop(mutex_cache_status);
                         drop(unlocked_cache);
                         let mut i: usize = 0;
@@ -203,8 +208,7 @@ fn run_bf(config: &ConfigStruct){
                         memory_pointer = add_to_usize(memory_pointer, cache.memory_pointer);
                         code_pointer = cache.code_pointer as usize;
 
-                    }
-                    else {
+                    } else { //Loop has already being cached or attempted to be cached, just do nothing and unlock the cache status
                         drop(mutex_cache_status);
                     }
                 }
@@ -216,12 +220,12 @@ fn run_bf(config: &ConfigStruct){
             memory.push(0);
         }
     }
-    log(&config,format!("\nBF excution done"),2);
+    log(&config,format!("\nBF execution done"),2);
     for handle in handles {
         handle.join().unwrap();
     }
 }
-fn get_inputs(input_raw: &String, config: &ConfigStruct)-> Vec<i64>{
+fn get_inputs(input_raw: &String, config: &ConfigStruct)-> Vec<i64>{ //Used in get_config to parse the inputs into a useable formate.
     let mut inputs: Vec<i64> = vec![];
     let input_split: Vec<&str> = input_raw.split(",").collect();
     for i in 0..input_split.len(){
@@ -235,7 +239,7 @@ fn get_inputs(input_raw: &String, config: &ConfigStruct)-> Vec<i64>{
 	}
     return inputs;
 }
-fn match_braces(config: &ConfigStruct)-> Vec<i32>{
+fn match_braces(config: &ConfigStruct)-> Vec<i32>{ //Match up the loop braces (Making sure that nested loops stay intact)
     let code_post = config.code.clone();
     let mut nested_level: i32 = 1;
     let mut bracket_left: Vec<Vec<i32>> = vec!();
@@ -265,7 +269,7 @@ fn match_braces(config: &ConfigStruct)-> Vec<i32>{
     return bracket_right;
 }
 
-fn get_config(args: &Vec<String>)-> ConfigStruct{
+fn get_config(args: &Vec<String>)-> ConfigStruct{ //Command line interface tool
     let mut config: ConfigStruct = ConfigStruct::new();
     let mut args_pointer: usize = 1;
     let mut arg: String;
@@ -279,22 +283,22 @@ EG:
 Switches:
     -h   | --help -> This help text
     -c   | --code-file -> Name/path of the BF code file
-    -i   | --inputs -> Preset inputs, comma seperated
+    -i   | --inputs -> Preset inputs, comma separated
     -dcc | --disable-code-comp -> Disables the code compression algorithm which will slow the program down
-    -dlc | --disable-loop-caching -> Disables the multithreaded Loop cache algorithm which will massivly slow 
-                                     the program down BUT in 1 in a 1,000,000 loops, the program will hang.
+    -dlc | --disable-loop-caching -> Disables the multi-threaded Loop cache algorithm which will massively slower
+                                     BUT in 1 in a 1,000,000 loops, the program will hang.
                                      So if your code is freezing, try using this option.
-    -oa  | --output-in-assic -> BF outputs are in assic (And inputs)
-    -od  | --output-in-decimal -> BF outputs are in decimal (Defualt, as it is the superior flavor)
+    -oa  | --output-in-ascii -> BF outputs are in ascii (And inputs)
+    -od  | --output-in-decimal -> BF outputs are in decimal (Default, as it is the superior flavor)
     -v   | --verbose -> All debug output
     -q   | --quiet -> Just the BF program output
     -s   | --silent -> No output at all
 
 Notes:
-    There is minimum error checking for maxium speed. You are on your own.
+    There is minimum error checking for maximum speed. You are on your own.
 
-    It is recomended to make your program in another tool, as this one dose not show your bf code memory,
-    but then you can run it using this one and watch it compute faster than what should be possible.
+    It is recommended to make your program in another tool, as this one dose not show your bf code memory,
+    but then, once done, you can run it using this one and watch it compute faster than what should be possible.
     ";
     if args.len() == 1 {
         log(&config,format!("{}",help_text),2);
@@ -307,14 +311,14 @@ Notes:
             "-h" | "--help" => {println!("{}",help_text); process::exit(0)},
             "-dcc" | "--disable-code-comp" => config.code_compression = false,
             "-dlc" | "--disable-loop-caching" => config.code_loop_cache = false,
-            "-oa" | "--output-in-assic" => config.output_type = 'a',
+            "-oa" | "--output-in-ascii" => config.output_type = 'a',
             "-od" | "--output-in-decimal" => config.output_type = 'd',
             "-c" | "--code-file" => {args_pointer+=1; config.code =  process_bf(&args[args_pointer].clone())},
             "-i" | "--inputs" => {args_pointer+=1; config.inputs = get_inputs(&args[args_pointer].clone(),&config);},
             "-v" | "--verbose" => config.print_level = 3,
             "-q" | "--quiet" => config.print_level = 1,
             "-s" | "--silent" => config.print_level = 0,
-            _ => throw_error(10, format!("Unkown option \"{}\"\n use -h --help to see posible options",&arg),&config)
+            _ => throw_error(10, format!("Unknown option \"{}\"\n use -h --help to see possible options",&arg),&config)
         }
         args_pointer+=1;
     }
@@ -330,33 +334,33 @@ fn throw_error(error_code: i32,message: std::string::String, config: &ConfigStru
     process::exit(error_code);
 }
 
-fn log(config: &ConfigStruct,message: String, log_level: i32) {
+fn log(config: &ConfigStruct,message: String, log_level: i32) { //For the silent, quiet, verbose tags to work.
     let global_log = config.print_level.clone();
     if log_level <= global_log {
         println!("{}",message);
     }
 }
-fn log_without_newline(config: &ConfigStruct,message: String, log_level: i32) {
+fn log_without_newline(config: &ConfigStruct,message: String, log_level: i32) {//Effectively same as above ^
     let global_log = config.print_level.clone();
     if log_level <= global_log {
         print!("{}",message);
     }
 }
-fn get_commandline_input (config: &ConfigStruct) -> i64 {
-    print!("Please enter input for program: ");
+fn get_commandline_input (config: &ConfigStruct) -> i64 { //When the BF code requests more inputs than user supplied on the commandline
+    log(&config,format!("Please enter input for program: "),2);
     let _=stdout().flush();
     let mut input = String::new();
     match io::stdin().read_line(&mut input) {
         Ok(_) => {
         }
-        Err(error) => println!("error: {}", error),
+        Err(error) => log(&config,format!("error: {}", error),1),
     }
     if config.output_type == 'a' {
         let mut result: i64 = 0;
         for current_char in input.trim().chars() {
             result += current_char as u8 as i64;
         }
-        println!("Assic to int -> {}", result);
+        log(&config,format!("ascii to int -> {}", result),2);
         return result;
     }
     else {
@@ -370,7 +374,7 @@ fn get_commandline_input (config: &ConfigStruct) -> i64 {
     }
 }
 
-fn add_to_usize(usize_num: usize, i32_num: i32) -> usize{
+fn add_to_usize(usize_num: usize, i32_num: i32) -> usize{ //Adding a negative number to a usize is not okay apparently to rust
     if i32_num.is_negative() {
         return usize_num - i32_num.wrapping_abs() as usize;
     } else {
@@ -380,7 +384,7 @@ fn add_to_usize(usize_num: usize, i32_num: i32) -> usize{
 
 #[derive(Debug)]
 #[derive(Clone)]
-pub struct LoopCacheMeta {
+pub struct LoopCacheMeta { //Data Obj for the loop cache algorithm
     instructions: Vec<Vec<i32>>,
     control_pointer: i32,
     code_pointer: i32,
